484cd01a91fbe86a3a7c9a91ef094bc8cec9617b
BDeslippe23 <bdeslippe@outlook.com>
2020-05-23 12:42:36 -0400

    Added login activity

--- app/src/main/java/com/example/liftoff/MainActivity.java
+++ app/src/main/java/com/example/liftoff/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.liftoff;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.liftoff.ui.main.MainFragment;
+import com.example.liftoff.ui.main.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +20,7 @@ public class MainActivity extends AppCompatActivity {
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow();
         }
+
     }
+
 }
